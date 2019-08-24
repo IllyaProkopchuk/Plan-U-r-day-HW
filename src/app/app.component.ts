7de13cdbce640cc5fa5813b0212d4c6e7b933125
Sirax19 <prokop4uk19@gmail.com>
2019-08-24 15:42:43 +0300

    HW(angular) plan u`r day

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { settings } from 'cluster';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HW2';
+  taskModule: string;
+  check: boolean;
+  saveIndex: number;
+  editTask: string;
+  users: Array<any> = [
+    // {
+    //   task: 'HTML',
+    //   check: true
+    // },
+    // {
+    //   task: 'Clean my room',
+    //   check: false
+    // },
+    // {
+    //   task: 'CSS',
+    //   check: true
+    // },
+    // {
+    //   task: 'do HW',
+    //   check: false
+    // }
+  ];
+
+  public addUser(): void {
+    if (this.taskModule == '' || this.taskModule == ' ' || this.taskModule == null) {
+      alert('U must enter some Value');
+    }
+    else {
+      let newUser = new Users(this.taskModule, this.check = true);
+      this.users.push(newUser);
+      this.taskModule = '';
+    }
+  };
+  public deleteUser(index: number): void {
+    this.users.splice(index, 1);
+  }
+  public editUser(index: number): void {
+    this.saveIndex = index;
+    this.editTask = this.users[index].task;
+    document.querySelector('.box').classList.remove('none');
+  }
+  public saveEdit(): void {
+    this.users[this.saveIndex].task = this.editTask;
+    document.querySelector('.box').classList.add('none');
+  }
 }
+
+interface IUser{
+  task: string;
+  check: boolean;
+}
+
+class Users implements IUser {
+  constructor(
+    public task: string,
+    public check: boolean
+  ){}
+};
